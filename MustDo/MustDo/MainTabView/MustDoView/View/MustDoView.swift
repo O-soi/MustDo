e93fe810eb8bb81986f9d1e645e6f8188df3106b
BeomKey <nkb5813@naver.com>
2022-02-20 19:16:34 +0900

    MustDoView 구현
    - onAppear시, 리스트 요청

--- MustDo/MustDo/MainTabView/MustDoView/View/MustDoView.swift
+++ MustDo/MustDo/MainTabView/MustDoView/View/MustDoView.swift
@@ -11,12 +11,27 @@ struct MustDoView: View {
     @StateObject private var viewModel = MustDoViewModel.factory()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                ForEach(viewModel.mustDoList) { mustDo in
+                    MustDoCell(mustDo: mustDo)
+                        .background(Color.white)
+                        .cornerRadius(15)
+                }
+            }
+            .padding()
+            .onAppear(perform: {
+                Task.detached(priority: .background) {
+                    try? await viewModel.requestMustDoList(userID: 0)
+                }
+            })
+        }
     }
 }
 
 struct MustDoView_Previews: PreviewProvider {
     static var previews: some View {
         MustDoView()
+            .background(Color.blue.opacity(0.1))
     }
 }
