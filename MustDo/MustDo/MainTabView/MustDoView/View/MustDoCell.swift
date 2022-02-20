8c867b3110cb80933213225729f972b52427aa94
BeomKey <nkb5813@naver.com>
2022-02-20 19:10:35 +0900

    MustDoCell, CircleGaugeView 파일 생성

---
+++ MustDo/MustDo/MainTabView/MustDoView/View/MustDoCell.swift
@@ -0,0 +1,20 @@
+//
+//  MustDoCell.swift
+//  MustDo
+//
+//  Created by 남기범 on 2022/02/20.
+//
+
+import SwiftUI
+
+struct MustDoCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MustDoCell_Previews: PreviewProvider {
+    static var previews: some View {
+        MustDoCell()
+    }
+}
---
+++ MustDo/MustDo/Utils/CircleGaugeView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleGaugeView.swift
+//  MustDo
+//
+//  Created by 남기범 on 2022/02/20.
+//
+
+import SwiftUI
+
+struct CircleGaugeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleGaugeView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleGaugeView()
+    }
+}
