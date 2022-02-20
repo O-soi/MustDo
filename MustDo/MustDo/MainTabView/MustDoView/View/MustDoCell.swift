2a1d1a211de20cf9b4b19cff8152c8d9a68cd002
BeomKey <nkb5813@naver.com>
2022-02-20 19:15:38 +0900

    MustDoCell 구현
    - 추가적인 데이터를 더 표시할지 고민해봐야함

--- MustDo/MustDo/MainTabView/MustDoView/View/MustDoCell.swift
+++ MustDo/MustDo/MainTabView/MustDoView/View/MustDoCell.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct MustDoCell: View {
+    let mustDo: MustDo
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            CircleGaugeView(progress: mustDo.gauge)
+                .frame(width: 60, height: 60, alignment: .center)
+                .padding()
+            
+            VStack(alignment: .leading, spacing: 5) {
+                makeMustDoInfoView(title: "해야할 일", dataString: mustDo.title)
+                makeMustDoInfoView(title: "소요 시간", dataString: "\(mustDo.elapsedSeconds)초")
+                makeMustDoInfoView(title: "수행 시간", dataString: "\(mustDo.performedSeconds)초")
+            }
+            .padding([.top, .bottom])
+            
+            Spacer()
+                
+        }
+    }
+}
+
+extension MustDoCell {
+    func makeMustDoInfoView(title: String, dataString: String) -> some View {
+        return HStack(alignment: .top) {
+            Text(title)
+                .font(.system(size: 10, weight: .bold))
+                .padding(5)
+                .background(Color.indigo)
+                .foregroundColor(Color.white)
+                .clipShape(Capsule())
+            
+            Text(dataString)
+                .font(.system(size: 15, weight: .bold))
+                .padding(.top, 2)
+        }
     }
 }
 
 struct MustDoCell_Previews: PreviewProvider {
     static var previews: some View {
-        MustDoCell()
+        MustDoCell(mustDo: dummyMustDo)
     }
 }
