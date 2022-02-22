e8b0868675b20f7b720c6f59117f2322a119dbb5
BeomKey <nkb5813@naver.com>
2022-02-22 23:22:01 +0900

    CircleTimerView 생성

---
+++ MustDo/MustDo/MainTabView/MustDoView/CircleTimerView/CirecleTimerView.swift
@@ -0,0 +1,20 @@
+//
+//  CirecleTimerView.swift
+//  MustDo
+//
+//  Created by 남기범 on 2022/02/22.
+//
+
+import SwiftUI
+
+struct CirecleTimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CirecleTimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        CirecleTimerView()
+    }
+}
