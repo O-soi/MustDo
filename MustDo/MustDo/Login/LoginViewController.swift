f98cc8ca25cd2debaaba7f401da040264c4c4222
BeomKey <nkb5813@naver.com>
2022-03-20 21:14:12 +0900

    다양한 로그인 방식을 도입을 고려해 네이밍 변경

---
+++ MustDo/MustDo/Login/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GoogleLoginViewController.swift
+//  MustDo
+//
+//  Created by 남기범 on 2022/03/20.
+//
+
+import Foundation
+import UIKit
+
+final class LoginViewController: UIViewController, GoogleLoginProtocol {
+    
+}
