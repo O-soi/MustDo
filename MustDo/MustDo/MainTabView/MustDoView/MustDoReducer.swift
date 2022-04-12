1df69fdb2f4552e2a5061c5b4f494a230f8b585e
BeomKey <nkb5813@naver.com>
2022-04-12 23:41:46 +0900

    MustDoAction Equatable 채택 및 function 구현

--- MustDo/MustDo/MainTabView/MustDoView/MustDoReducer.swift
+++ MustDo/MustDo/MainTabView/MustDoView/MustDoReducer.swift
@@ -11,20 +11,37 @@ import ComposableArchitecture
 
 struct MustDoState: Equatable {
     static func == (lhs: MustDoState, rhs: MustDoState) -> Bool {
-        let count = lhs.mustDoList.filter { mustDo in
-            rhs.mustDoList.filter { $0.id == mustDo.id }.count == 1
-        }.count
-        
-        return lhs.mustDoList.count == count
-        && lhs.mustDoList.count == rhs.mustDoList.count
+        return lhs.mustDoList == rhs.mustDoList
     }
     
     var mustDoList: [MustDo] = []
 }
 
-enum MustDoAction {
+enum MustDoAction: Equatable {
+    static func == (lhs: MustDoAction, rhs: MustDoAction) -> Bool {
+        switch (lhs, rhs) {
+        case let (.requestMustDoList(userID: lhsID), .requestMustDoList(userID: rhsID)):
+            return lhsID == rhsID
+        case let (.mustDoListResponse(.success(lhsResult)), .mustDoListResponse(.success(rhsResult))):
+            return lhsResult == rhsResult
+        default:
+            return false
+        }
+    }
+    
     case requestMustDoList(userID: Int)
     case mustDoListResponse(Result<[MustDo], Error>)
+}
+
+extension Array where Element == MustDo {
+    static func == (lhs: [MustDo], rhs: [MustDo]) -> Bool {
+        let count = lhs.filter { mustDo in
+            rhs.filter { $0.id == mustDo.id }.count == 1
+        }.count
+        
+        return lhs.count == count
+        && lhs.count == rhs.count
+    }
 }
 
 struct MustDoEnvironment {
