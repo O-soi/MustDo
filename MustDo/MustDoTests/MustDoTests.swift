257c4edbd468e902c7fbd54a4b9a38ab2e0dfd6c
BeomKey <nkb5813@naver.com>
2022-04-12 23:42:36 +0900

    unit test에 pod 설치 및 mustDo test case 작성

--- MustDo/MustDoTests/MustDoTests.swift
+++ MustDo/MustDoTests/MustDoTests.swift
@@ -6,27 +6,52 @@
 //
 
 import XCTest
+import Combine
+import ComposableArchitecture
+
+@testable import MustDo
 
 class MustDoTests: XCTestCase {
+    let scheduler = DispatchQueue.test
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    func test_request_mustDoList() {
+        let store = TestStore(
+            initialState: MustDoState(),
+            reducer: mustDoReducer,
+            environment: MustDoEnvironment(
+                queue: scheduler.eraseToAnyScheduler(),
+                interactor: MustDoInteractor(repository: StubMustDoRepository())
+            )
+        )
+        
+        store.send(.requestMustDoList(userID: 0))
+        self.scheduler.advance()
+        store.receive(.mustDoListResponse(.success([dummyMustDo]))) {
+            $0.mustDoList = [dummyMustDo]
         }
     }
+}
 
+class StubMustDoRepository: MustDoRepositoryProtocol {
+    func requestMustDoList(userID: Int) -> Future<[MustDo], Error> {
+        return Future { promise in
+            promise(.success([dummyMustDo]))
+        }
+    }
+    
+    func addMustDo(userID: Int) {
+        
+    }
+    
+    func updateMustDo(userID: Int, percent: Double) {
+        
+    }
 }
