55c8763ef72a8983e0eaa05da2b69fce0a20ed9f
BeomKey <nkb5813@naver.com>
2022-12-27 08:02:04 +0900

    프로젝트 생성

---
+++ MustDo/MustDo/MustDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MustDoApp.swift
+//  MustDo
+//
+//  Created by 남기범 on 2022/12/27.
+//
+
+import SwiftUI
+
+@main
+struct MustDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
